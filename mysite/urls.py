abea000399ce758ecd2d02a42c8d8f63f74f663a
Steven Pilatschek <steven.pilatschek@gmail.com>
2018-03-12 12:14:30 +0100

    Completed part 3 of the tutorial
    Added first advanced views

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+app_name = 'polls'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
