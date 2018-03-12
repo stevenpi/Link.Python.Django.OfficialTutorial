06be10b8ef7d93c4aab7a0ba21abb307aa7d504a
Steven Pilatschek <steven.pilatschek@gmail.com>
2018-03-12 13:49:52 +0100

    Completed Beginner Tutorial
    Changed default template of the admin site

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 
 from polls.models import Question, Choice
 
-admin.site.register(Question)
 admin.site.register(Choice)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
